3a4617432c403edf61205a7db8868fa9f3b66ae2
huifenglan <huifeng1@vt.edu>
2021-11-28 21:44:04 -0500

    add look up fun

---
+++ ClientSide/LookupFunc.ps1
@@ -0,0 +1,8 @@
+function LookupFunc{
+	## moduleName could be user32.dll, and the functionName would be MessageBoxA from pervious example
+	Param ($moduleName, $functionName)
+	$assem = ([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object {$_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].Equals('System.dll')}).GetType('Microsoft.Win32.UnsafeNativeMethods')
+	$tmp=@()
+	$assem.GetMethods() | ForEach-Object {If($_.Name -eq "GetProcAddress") {$tmp+=$_}}
+	return $tmp[0].Invoke($null, @(($assem.GetMethod('GetModuleHandle')).Invoke($null, @($moduleName)), $functionName))
+}
