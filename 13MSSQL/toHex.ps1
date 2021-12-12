2bf6d3a090b2fcddb57436a246347a166fde580a
huifenglan <huifeng1@vt.edu>
2021-12-11 21:47:49 -0500

    add to hexps1

---
+++ 13MSSQL/toHex.ps1
@@ -0,0 +1,9 @@
+$assemblyFile = "cmdExec.dll"
+$stringBuilder = New-Object -Type System.Text.StringBuilder
+$fileStream = [IO.File]::OpenRead($assemblyFile)
+
+while (($byte = $fileStream.ReadByte()) -gt -1) {
+  $stringBuilder.Append($byte.ToString("X2")) | Out-Null
+}
+
+$stringBuilder.ToString() -join "" | Out-File c:\Tools\cmdExec.txt
